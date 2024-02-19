9634b60ea954a237418d4af79697d0c11902d69e
anilyazan <anilyazan99@gmail.com>
2024-02-19 17:26:56 +0300

    shopping fixed

--- components/ImageGallery.js
+++ components/ImageGallery.js
@@ -1,17 +1,41 @@
 // components/ImageGallery.js
+
+import React, { useState } from "react";
+
 const ImageGallery = ({ images }) => {
-  console.log(images);
+  const [selectedImage, setSelectedImage] = useState(images[0]);
+
+  const handleThumbnailClick = (image) => {
+    setSelectedImage(image);
+  };
+
   return (
-    <div className="grid grid-cols-4 gap-4">
-      {images &&
-        images.map((image, index) => (
-          <img
-            key={index}
-            src={image}
-            alt={`Image ${index}`}
-            className="w-full h-auto"
-          />
-        ))}
+    <div className="flex">
+      <div className="flex-1">
+        <img
+          src={selectedImage}
+          alt="Selected Image"
+          style={{ maxWidth: "400px", maxHeight: "800px", margin: "5px" }}
+        />
+      </div>
+      <div className="flex-1">
+        <div className="grid grid-cols-4 gap-4">
+          {images.map((image, index) => (
+            <img
+              key={index}
+              src={image}
+              alt={`Image ${index}`}
+              style={{
+                cursor: "pointer",
+                maxWidth: "100px",
+                maxHeight: "100px",
+                margin: "5px",
+              }}
+              onClick={() => handleThumbnailClick(image)}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
